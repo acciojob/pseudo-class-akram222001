52fe10effdf36e2bf900196c06d9b87ca50d1c6b
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2024-02-28 03:33:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+#container{
+	margin:20px;
+	 position: relative;
+}
+#container::before {
+    content: "Before";
+    position: absolute;
+    top: -30px;
+    left: 0;
+    color: #f00;
+    font-size: 20px;
+}
+
+#container::after {
+    content: "After";
+    position: absolute;
+    bottom: -30px;
+    right: 0;
+    color: #f00;
+    font-size: 20px;
+}
